2c4a68c8c6779217cc14cddd11c7cff34cb16355
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-10 07:40:59 -0700

    Added TODO prompts

---
+++ hello_again.c
@@ -0,0 +1,23 @@
+/*
+  AP CSP
+  Instructor: C. Waage
+  Date: 10/10/2018
+
+  Follow the TODO prompts below to add user input 
+  to this code, and 'echo' that output using a %s 
+  placeholder in a printf() statement. 
+*/
+#include<stdio.h>
+// TODO: Add an include for cs50.h
+
+
+int main(void)
+{
+  // Create a string variable called name
+  // TODO: Get User's name with the get_string() function
+  
+  // TODO: Change the printf() below
+  // Use the '%s' placeholder to output "Hello name" (where name is the value input by user).
+  // Be sure to include a new line character '\n'
+  printf("hello, world");
+}
