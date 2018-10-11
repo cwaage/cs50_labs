74e04849b874051ffa5191aea4401899662337a8
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-11 11:24:26 -0700

    Changed hello.c to fahrenheit.c

---
+++ fahrenheit.c
@@ -0,0 +1,20 @@
+/*
+Author: 
+Date: 
+Description: This program converts Celcius temperatures to Fahrenheit
+
+Concepts:
+    Input / Output
+    Variable Type
+    Math Operators
+    Order of Precedence
+    printf(Formatting Decimals)
+*/
+
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+    
+}
