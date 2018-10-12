193a836e0f6f9a65d7a9545cdc1137bd329ec437
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-12 09:03:01 -0700

    Added decimal printing of e

--- unit1_practice_lab_0.c
+++ unit1_practice_lab_0.c
@@ -3,11 +3,13 @@
 
 int main(void)
 {
-  // TODO: Create three variables, a float, an int, and a string
+  // TODO 0: Create three variables, a float, an int, and a string
   // Assign user input to each of them using the get_float(), get_int(), and get_string() methods
   
-  // TODO: Create int variables to assign each of the equations listed in the practice guide
+  // TODO 1: Create int variables to assign each of the equations listed in the practice guide
   
-  // TODO: Print each of the variables as shown in the guide
+  // TODO 2 : Print each of the variables as shown in the guide
+  
+  // TODO 3 : Print the e variable to the number of decimal places specified in the guide
  
 }
