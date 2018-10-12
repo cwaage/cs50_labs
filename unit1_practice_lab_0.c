1fb2464e3fe72d234e5bbb59e6d6b90826d05e40
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-12 08:47:07 -0700

    Changed to unit1_practice_lab_0.c added TODOs

---
+++ unit1_practice_lab_0.c
@@ -0,0 +1,13 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+  // TODO: Create three variables, a float, an int, and a string
+  // Assign user input to each of them using the get_float(), get_int(), and get_string() methods
+  
+  // TODO: Create int variables to assign each of the equations listed in the practice guide
+  
+  // TODO: Print each of the variables as shown in the guide
+ 
+}
