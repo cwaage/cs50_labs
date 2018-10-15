e2d8c1683dce89bc7c9ce94c52ac5dab0755f3f2
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-15 05:57:27 -0700

    Commented Out Lower Sections

---
+++ practice_lab_1.c
@@ -0,0 +1,122 @@
+/*
+ * Conditional Branching
+ * Boolean Operators 
+ * Loops
+ * Accumulating Values 
+ * /
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{    
+    // ------------------------------------------
+    // Conditionals
+    // ------------------------------------------
+    
+    int num1 = 20;
+    int num2 = 30;
+    
+    /* UNCOMMENT CODE TO COMPLETE (REMOVE THIS LINE AND LINE 33
+    if() 
+    {
+        printf("num1 is greater than num2\n");
+    }
+    // TODO: code 'else if' with proper syntax
+    
+        printf("num2 is greater than num1\n");
+    
+    // TODO: code 'else' with proper syntax
+    
+        printf("num1 and num2 are equal\n");
+        
+     REMOVE THIS LINE OF CODE TO UNCOMMENT Conditionals Section */  
+    
+    // ------------------------------------------
+    // Boolean Expressions 
+    // ------------------------------------------
+
+    int x = 5;
+    int y = 2;
+    int z = 8; 
+
+    /* UNCOMMENT CODE TO COMPLETE (REMOVE THIS LINE AND LINE 59
+
+    if ()
+    {
+        printf("x is the maximum\n");
+    }
+    else if ()
+    {
+        printf("z is the maximum\n");
+    }
+    else
+    {
+        // Fill in printf with proper message 
+        printf("\n");
+    }
+    
+    REMOVE THIS LINE OF CODE TO UNCOMMENT Boolean Expression Section  */
+
+    // ----------------------------------------
+    // Loops
+    // ----------------------------------------
+    
+    // ----------------------------------------
+    // do while loop
+    // Always enter loop block at least once
+    // Repeat while the boolean expression is TRUE
+    // ----------------------------------------
+  
+    /* UNCOMMENT CODE TO COMPLETE (REMOVE THIS LINE AND NEXT LINE WITH CLOSING COMMENT)
+    do
+    {
+ 
+    } while();
+    // ----------------------------------------
+    
+    // ----------------------------------------
+    // while loop
+    // Only Enter and Repeat when boolean expression is TRUE
+    // ----------------------------------------
+  
+
+    while ()
+    {
+ 
+    }
+    // ----------------------------------------
+
+
+    // ----------------------------------------
+    // for loop
+    // ----------------------------------------
+    // Repeat Loop 8 times
+    for 
+    {
+        printf
+    }
+
+    REMOVE THIS LINE OF CODE TO UNCOMMENT LOOP SECTION */
+
+    // ----------------------------------------
+    // Accumulation Value
+    // ----------------------------------------
+
+
+    /* UNCOMMENT CODE TO COMPLETE (REMOVE THIS LINE AND NEXT LINE WITH CLOSING COMMENT)
+    // Reassign z (declared above)
+    z = 3;
+
+    // Create For Loop 
+    for
+    {
+        // TODO: Code to accumulate Variable 
+    }
+
+    // TODO Uncomment the code below to print the z variable to display its final value
+    // printf("After Accumulating Block Executes z = %i\n", z);
+    
+    REMOVE THIS LINE OF CODE TO UNCOMMENT Accumulating Value Section */
+    
+}
