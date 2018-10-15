84df6af54639698ebb529f6a09d395c53d7ef5bf
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-14 18:01:24 -0700

    Remove math.h

--- pennies.c
+++ pennies.c
@@ -1,30 +1,28 @@
 #include<cs50.h>
 #include<stdio.h>
-// TODO 0
-// Add math header file, math.h 
 
 int main(void)
 {
-  // TODO 1 : Declare Your Variables per below
+  // TODO 0 : Declare Your Variables per below
   //  A. Integer variable to capture number of days in the month 
   //  B. Integer variable to capture starting number of pennies  
   
-  // TODO 2 : Create a loop block that
+  // TODO 1 : Create a loop block that
   //  A. Executes at least once
   //  B. Assigns variable for number of days from user input 
   //  C. Repeats Block (Loops) when the input value is less than 28 or more than 31
   
-  // TODO 3 : Create a loop block that
+  // TODO 2 : Create a loop block that
   //  A. Executes at least once
   //  B. Assigns your variable for starting number of pennies from user input 
   //  C. Repeats Block (Loops) when the input value is less than 1
   
-  // TODO 4 : Calculate total value after the number of days
+  // TODO 3 : Calculate total value after the number of days
   // Declare a long long variable
   // Assign that variable the calculated total number of pennies
   // Note: Use the pow() function from the math.h library in your equation. 
 
-  // TODO 5 : Convert to currancy format
+  // TODO 4 : Convert to currancy format
 
-  // TODO 6 : Print with two decimal places for correct currency format (i.e. '$4.27)
+  // TODO 5 : Print with two decimal places for correct currency format (i.e. '$4.27)
 }
